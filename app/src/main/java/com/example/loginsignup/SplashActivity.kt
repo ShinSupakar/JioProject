49360d0ffae4193cc03412ebe269a6f393ef03dc
milibanerjee <banerjee.swarnika27@gmail.com>
2023-07-04 13:36:38 +0530

    splash screen + login screen

---
+++ app/src/main/java/com/example/loginsignup/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.loginsignup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,LoginActivity2::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
+
+
