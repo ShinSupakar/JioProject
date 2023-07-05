f147930864395a2230d2eb98a623f8e69c183700
ShinSupakar <shinjinisupakar@gmail.com>
2023-07-05 11:09:25 +0530

    new updates

--- app/src/main/java/com/example/loginsignup/MainActivity.kt
+++ app/src/main/java/com/example/loginsignup/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.loginsignup
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.loginsignup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val blockList = listOf("BLOCK 1" +
+                "                                                >", "BLOCK 2 " +
+                "                                               >", "BLOCK 3 " +
+                "                                               >", "BLOCK 4 " +
+                "                                               >", "BLOCK 5 " +
+                "                                               >")
+        val adapter = BlockAdapter(blockList) { position ->
+            val intent = Intent(this, NextActivity::class.java)
+            intent.putExtra("blockNumber", position + 1)
+            startActivity(intent)
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
     }
 }
